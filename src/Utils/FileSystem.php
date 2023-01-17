6e5b526597851771a01db8469201b93d8618af05
David Grudl <david@grudl.com>
2023-01-17 23:26:59 +0100

    added FileSystem::unixSlashes() & platformSlashes()

--- src/Utils/FileSystem.php
+++ src/Utils/FileSystem.php
@@ -258,4 +258,24 @@ final class FileSystem
 	{
 		return self::normalizePath(implode('/', $paths));
 	}
+
+
+	/**
+	 * Converts backslashes to slashes.
+	 */
+	public static function unixSlashes(string $path): string
+	{
+		return strtr($path, '\\', '/');
+	}
+
+
+	/**
+	 * Converts slashes to platform-specific directory separators.
+	 */
+	public static function platformSlashes(string $path): string
+	{
+		return DIRECTORY_SEPARATOR === '/'
+			? strtr($path, '\\', '/')
+			: str_replace(':\\\\', '://', strtr($path, '/', '\\')); // protocol://
+	}
 }
